bac7b213e0464fdd3c0b6a5a82408b956bf1741f
Vishakha Sahu <vishakhasahu023@gmail.com>
2023-01-04 12:51:30 +0530

    initial setup commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import './App.css'
 
-function App() {
+const App = () => {
+  const [number, setNumber] = useState(1)
+  const [title, setTitle] = useState('')
+  const [image, setImage] = useState('')
+  const [description, setDescription] = useState('')
+
+  async function getstoreData() {
+    const response = await axios.get(`https://fakestoreapi.com/products/${number}`)
+    // console.log(response)
+    setTitle(response.data.title)
+    setImage(response.data.image)
+    setDescription(response.data.description)
+
+  }
+  getstoreData()
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>Our Shop</h1>
+      <div className='app'>
+        <select value={number} onChange={(e) => setNumber(e.target.value)}>
+          <option value='1'>1</option>
+          <option value='2'>2</option>
+          <option value='3'>3</option>
+          <option value='4'>4</option>
+          <option value='5'>5</option>
+          <option value='6'>6</option>
+          <option value='7'>7</option>
+          <option value='8'>8</option>
+          <option value='9'>9</option>
+          <option value='10'>10</option>
+        </select>
+        <h1>Product {number} listed here is: <br /><span>{title}</span> </h1>
+        <div className='product'>
+          <h2>{title}</h2>
+          <img src={image} />
+          <p>{description}</p>
+        </div>
+
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
